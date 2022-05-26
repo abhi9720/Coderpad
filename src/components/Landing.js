35989fed3bbc29c5328160da504fcc7e0ff40968
abhi9720 <abhishektiwari9720@gmail.com>
2022-05-26 23:58:27 +0530

    button to run code implemented

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -278,7 +278,7 @@ const Landing = () => {
 
 
                     <button onClick={downloadTxtFile} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center">
-                        CTRL+S - Save Code
+                        CTRL+q - Save Code
                     </button>
 
                     <button onClick={resetCode} type="button" class="text-white bg-[#FF9119] hover:bg-[#FF9119]/80 focus:ring-4 focus:outline-none focus:ring-[#FF9119]/50 font-medium rounded-lg text-sm px-5 py-2 text-center inline-flex items-center dark:hover:bg-[#FF9119]/80 dark:focus:ring-[#FF9119]/40 mr-2 mb-2">
